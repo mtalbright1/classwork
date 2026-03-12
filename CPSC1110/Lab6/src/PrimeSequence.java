a42566ea578f80bb535151ae862d7120236ad6d7
mtalbright1 <mtalbright1@gmail.com>
2026-03-12 15:50:50 -0400

    small changes to lab6

--- CPSC1110/Lab6/src/PrimeSequence.java
+++ CPSC1110/Lab6/src/PrimeSequence.java
@@ -1,27 +1,25 @@
 
 public class PrimeSequence implements Sequence {
-    private int n = 2;
+    private int n;
+    private boolean isPrime;
+    
+    public PrimeSequence() {
+    	n = 0;
+    }
     
     public int next() {
-    	
-    	if (n == 2) {
-    		return n++;
-    	}
-    	
-    	n++;
-    	
-    	while (true) {
-    		boolean isPrime = true;
-    		for (int i = 2; i < n; i++) {
+    	while(true) {
+    		isPrime = true;
+    		n++;
+    		for (int i = 2; i <= n/2; i++) {		// skip 1, only needs to go up to half n
     			if (n % i == 0) {
     				isPrime = false;
-    				break;
+    				break;							// not a prime, exit for loop
     			}
     		}
     		if (isPrime == true) {
     			return n;
     		}
-    		n++;
     	}
     }
 }

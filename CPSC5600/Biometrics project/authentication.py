49fe775fa851314862c357bfb43cf367050f8d51
mtalbright1 <mtalbright1@gmail.com>
2026-04-17 11:08:07 -0400

    did a little for authentication.py

--- CPSC5600/Biometrics project/authentication.py
+++ CPSC5600/Biometrics project/authentication.py
@@ -3,7 +3,14 @@
 import biometric
 import encryption
 
-fingerprint_path = input("Provide filepath to input fingerprint: ")
+match_threshold = 75
+fingerprint_database_path = "Fingerprint database"
+input_fingerprint_path = input("Provide filepath to input fingerprint: ")       # prompt the user for input
 
+similarity_score = biometric.compare_fingerprints(input_fingerprint_path, fingerprint_database_path)
 
+if similarity_score < match_threshold:
+    print("Authentication failed: match not found")
+    exit()
 
+print("Authentication successful")
--- CPSC5600/Biometrics project/biometric.py
+++ CPSC5600/Biometrics project/biometric.py
@@ -3,59 +3,63 @@
 import os       # for accessing files
 import cv2      # computer vision, install with $pip install opencv-python
 
-# can download fingerprint .bmp's from SOCOFing or FVC2002
-stored_fingerprints = "Fingerprint database"
-input_fingerprint = cv2.imread("Fingerprint database/1__M_Left_index_finger.BMP")
-#input_fingerprint = cv2.resize(input_fingerprint, None, fx=2.5, fy=2.5)     # scale up image size, if needed
+def compare_fingerprints(input_fingerprint_path: str, stored_fingerprint_path: str) -> float:
+    # can download fingerprint .bmp's from SOCOFing or FVC2002
+    stored_fingerprints = stored_fingerprint_path
+    input_fingerprint = cv2.imread(input_fingerprint_path)
+    #input_fingerprint = cv2.resize(input_fingerprint, None, fx=2.5, fy=2.5)     # scale up image size, if needed
 
-best_score = 0      # similarity score for the current best match
-match_filename = None      # will be filled with best match
-match_image = None
-keypoints_input = None      # keypoints are conspicuous parts of an image
-keypoints_stored = None
-match_keypoints = None
+    best_score = 0      # similarity score for the current best match
+    match_filename = None      # will be filled with best match
+    match_image = None
+    keypoints_input = None      # keypoints are conspicuous parts of an image
+    keypoints_stored = None
+    match_keypoints = None
 
-for file in os.listdir(stored_fingerprints):     # loop through the stored fingerprints ('file' is just a string of the file's name)
-    full_path = os.path.join(stored_fingerprints, file)     # path of directory + file
-    fingerprint_image = cv2.imread(full_path)
-    sift = cv2.SIFT_create()        # sift object that creates keypoints and descriptors from an image
+    for file in os.listdir(stored_fingerprints):     # loop through the stored fingerprints ('file' is just a string of the file's name)
+        full_path = os.path.join(stored_fingerprints, file)     # path of directory + file
+        fingerprint_image = cv2.imread(full_path)
+        sift = cv2.SIFT_create()        # sift object that creates keypoints and descriptors from an image
 
-    keypoints_1, descriptors_1 = sift.detectAndCompute(input_fingerprint, None)     # descriptors describe keypoints
-    keypoints_2, descriptors_2 = sift.detectAndCompute(fingerprint_image, None)     # a 128-number vector describing what the local area around the keypoint looks like
+        keypoints_1, descriptors_1 = sift.detectAndCompute(input_fingerprint, None)     # descriptors describe keypoints
+        keypoints_2, descriptors_2 = sift.detectAndCompute(fingerprint_image, None)     # a 128-number vector describing what the local area around the keypoint looks like
 
-    matches = cv2.FlannBasedMatcher({'algorithm': 1, 'trees': 10}, {}).knnMatch(descriptors_1, descriptors_2, k=2)
-    # Fast Library for Approximate Nearest Neighbor
-    # algorithm 1 is KD-tree with 10 trees. Searching for 'k' nearest neighbors
+        matches = cv2.FlannBasedMatcher({'algorithm': 1, 'trees': 10}, {}).knnMatch(descriptors_1, descriptors_2, k=2)
+        # Fast Library for Approximate Nearest Neighbor
+        # algorithm 1 is KD-tree with 10 trees. Searching for 'k' nearest neighbors
 
-    matched_points = []
-    for i, j in matches:        # 'i' is best match, 'j' is second best. If 'i' is a significantly better match than 'j', we are confident it is a correct match
-        if i.distance < 0.7 * j.distance:       # Lowe's ratio test - ratio can be adjusted, lower is stricter. Distance is difference from input
-            matched_points.append(i)
+        matched_points = []
+        for i, j in matches:        # 'i' is best match, 'j' is second best. If 'i' is a significantly better match than 'j', we are confident it is a correct match
+            if i.distance < 0.7 * j.distance:       # Lowe's ratio test - ratio can be adjusted, lower is stricter. Distance is difference from input
+                matched_points.append(i)
 
-    keypoints = 0
-    if len(keypoints_1) < len(keypoints_2):     # get the one with fewer keypoints
-        keypoints = len(keypoints_1)
-    else:
-        keypoints = len(keypoints_2)
+        keypoints = 0
+        if len(keypoints_1) < len(keypoints_2):     # get the one with fewer keypoints
+            keypoints = len(keypoints_1)
+        else:
+            keypoints = len(keypoints_2)
 
-    if keypoints == 0:      # just avoiding divide by zero in case of improper input
-        continue
+        if keypoints == 0:      # just avoiding divide by zero in case of improper input
+            continue
 
-    if len(matched_points) / keypoints * 100 > best_score:      # is the percent of matching keypoints the highest so far?
-        best_score = len(matched_points) / keypoints * 100
-        match_filename = file       # save this one as the best match
-        match_image = fingerprint_image
-        keypoints_input = keypoints_1
-        keypoints_stored = keypoints_2
-        match_keypoints = matched_points
+        if len(matched_points) / keypoints * 100 > best_score:      # is the percent of matching keypoints the highest so far?
+            best_score = len(matched_points) / keypoints * 100
+            match_filename = file       # save this one as the best match
+            match_image = fingerprint_image
+            keypoints_input = keypoints_1
+            keypoints_stored = keypoints_2
+            match_keypoints = matched_points
 
-# output for testing
-print("Best match: " + match_filename)
-print("Score: " + str(best_score))
+    # output for testing
+    # print("Best match: " + match_filename)
+    # print("Score: " + str(best_score))
 
-# visualize the matching
-result = cv2.drawMatches(input_fingerprint, keypoints_input, match_image, keypoints_stored, match_keypoints, None)
-result = cv2.resize(result, None, fx=3, fy=3)     # resize the image
-cv2.imshow("Result", result)
-cv2.waitKey(0)      # wait forever until I press any key
-cv2.destroyAllWindows()     # then close all windows
+
+    # visualize the matching
+    result = cv2.drawMatches(input_fingerprint, keypoints_input, match_image, keypoints_stored, match_keypoints, None)
+    result = cv2.resize(result, None, fx=3, fy=3)     # resize the image
+    cv2.imshow("Result", result)
+    cv2.waitKey(0)      # wait forever until I press any key
+    cv2.destroyAllWindows()     # then close all windows
+
+    return best_score
